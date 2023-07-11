e2123e41fdc6b229bc30d241699871681982c8b4
laksh2605 <69389577+laksh2605@users.noreply.github.com>
2023-07-11 21:04:11 +0530

    admin add

---
+++ Addadmins.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add New Admin</title>
+</head>
+<body>
+    <h1>Add New Admin to System</h1>
+    <form action='addadminsql.php' method = "POST">
+        Forename: <input type = "text" name= "forename"><br>
+        Surname: <input type = "text" name= "surname"><br>
+        Username: <input type = "text" name= "username"><br>
+        Password: <input type = "text" name= "password"><br>
+        Email_Address: <input type = "text" name= "emailaddress"><br>
+        <input type="submit" value="Add New Admin><br>
+</form>
+</body>
+</html>
