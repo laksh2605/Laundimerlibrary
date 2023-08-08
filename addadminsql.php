747545ea3b70d24c1931402f21a44994eb3199b3
laksh2605 <laksh_26@icloud.com>
2023-08-08 08:30:03 +0100

    addadmin page

--- addadminsql.php
+++ addadminsql.php
@@ -1,28 +1,19 @@
 <?php
-header('Location: Addadmins.php');
-try{
-	include_once('connection.php');
-	array_map("htmlspecialchars", $_POST);
-    
-	$stmt = $conn->prepare("INSERT INTO TblUsers
-    (Surname,Forename,Username,Password,Email_address,UserRole)VALUES
-    (NULL,:surname,:forename,:username,:password,;emailaddress,:userrole)");
 
-    //this takes the unique inputs from the Addbooks.php page and then binds them to the general parameters above
-    $stmt->bindParam(':surname', $_POST["surname"]);
-    $stmt->bindParam(':forename', $_POST["forename"]);
-	$stmt->bindParam(':username', $_POST["username"]);
-    $stmt->bindParam(':password', $_POST["password"]);
-    $stmt->bindParam(':emailaddress', $_POST["emailaddress"]);
-    $stmt->bindParam(':userrole', $_POST["userrole"]);
-	$stmt->execute();
 
+include_once('connection.php');
+
+echo "Connected Sucessfully";
+
+$sql = "INSERT INTO tblusers
+(Surname,Forename,Username,Password,Email_Address,UserRole)VALUES
+('".$_POST["surname"]."','".$_POST["forename"]."','".$_POST["username"]."','".$_POST["password"]."','".$_POST["emailaddress"]."','".$_POST["userrole"]."')";
+
+if($conn->query($sql) == TRUE){
+    echo "Successful insertion";
 }
-catch(PDOException $e)
 
-{
-$conn=null;
-}
+
 
 ?>
 
--- connection.php
+++ connection.php
@@ -1,19 +1,8 @@
 <?php
-//this gives all the appropriate information a set variable
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "Laundimerlibrary";
+$server = "127.0.0.1";
+$user = "root";
+$pass = "";
+$dbname = "laundimerlibrary";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "Connected successfully <br>"; 
-    }
-    //again this outputs if the database connection failed and shows why
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage()."<br>";
-    }
+$conn = new mysqli($server, $user, $pass, $dbname);
 ?>
