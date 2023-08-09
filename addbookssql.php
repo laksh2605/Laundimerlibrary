0f57906bc28937961af3e61966578d4f4b4f313a
laksh2605 <laksh_26@icloud.com>
2023-08-09 10:39:36 +0100

    books done

--- addbookssql.php
+++ addbookssql.php
@@ -1,13 +1,22 @@
 <?php
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "laundimerlibrary";
 
-include_once('connection.php');
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-echo "Connected Sucessfully";
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
 $sql = "INSERT INTO tblbooks
-(ISBN,Title,Author,Genre,Description,PageLength,Rating,In_Library)VALUES
-('".$_POST["ISBN"]."','".$_POST["Title"]."','".$_POST["Author"]."','".$_POST["Genre"]."','".$_POST["Description"]."','".$_POST["PageLength"]."','".$_POST["Rating"]."','".$_POST["In_Library"]."')";
+(ISBN,Title,Author,Genre,Description,Length,Rating,In_Library)VALUES
+('".$_POST["ISBN"]."','".$_POST["Title"]."','".$_POST["Author"]."','".$_POST["Genre"]."','".$_POST["Description"]."','".$_POST["Length"]."','".$_POST["Rating"]."','".$_POST["In_Library"]."')";
 
 if($conn->query($sql) == TRUE){
     echo "Successful insertion";
--- navbar.php
+++ navbar.php
@@ -1,15 +1,15 @@
-<HTML>
+<html>
 <div class="topMenu">
             <button class="menubutton" onclick="document.location.href='Addbooks.php'">Add Books</button>
             <button class="menubutton" onclick="document.location.href='Addadmins.php'">Sign Up</button>
             <button class="menubutton" type="button">Login</button>
         </div>
         <div class="middlebar">
-            <H1>Library</H1>
+            <H1>Laundimer Library</H1>
         </div>
         <div class="lowerMenu">
             <button class="menubutton" type="button">About</button>
             <button class="menubutton" type="button">Browse Catalogue</button>
             <button class="menubutton" type="button">New Requests</button>
         </div>
-</HTML>
+</html>
