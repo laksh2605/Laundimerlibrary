abc483bbeadac219387362e4ee7f8e2a91265087
laksh2605 <laksh_26@icloud.com>
2023-09-27 12:05:14 +0100

    ok

--- viewloanhistorysql.php
+++ viewloanhistorysql.php
@@ -1,25 +1,23 @@
 <?php
+include_once("connection.php"); // Include your connection settings
 
-include_once ("connection.php");
+$sql = "INSERT INTO tblloans (UserID, ISBN, Date_Borrowed, Date_Returned,) VALUES (?, ?, ?, ?,)";
 
-    $stmt=$conn->prepare("INSERT INTO tblloans
-    (UserID,ISBN,Date_Borrowed,Date_Returned,Review,Rating,Late_Fines)VALUES(NULL,:isbn,:date_borrowed,:date_returned,:review,:rating,:late_fines)");
+$stmt = $conn->prepare($sql);
 
-    $stmt-> bindParam(':UserID', $_POST["UserID"]);
-    $stmt-> bindParam(':isbn', $_POST["ISBN"]);
-    $stmt-> bindParam(':date_borrowed', $_POST["date_borrowed"]);
-    $stmt-> bindParam(':date_returned', $_POST["date_returned"]);
-    $stmt-> bindParam(':rating', $_POST["rating"]);
-    $stmt-> bindParam(':late_fines', $_POST["late_fines"]);
-    $stmt->execute();
-    
+// Assuming $_POST["UserID"], $_POST["ISBN"], $_POST["Date_Borrowed"], $_POST["Date_Returned"] are the form input values.
 
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-      } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-      }
-      
-      $conn->close();
-    
+$stmt->bind_param('sssssss', $_POST["UserID"], $_POST["ISBN"], $_POST["Date_Borrowed"], $_POST["Date_Returned"]);
+
+if ($stmt->execute()) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
 ?>
+
+
+
